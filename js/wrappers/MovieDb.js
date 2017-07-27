ad7077b939e17d4a09bd5e7105a6ad0960b242d0
mazikwyry <a.mazur@exlabs.co.uk>
2017-07-27 12:13:45 +0200

    Setup test env with mocha and create specs for SearchMovie service and MovieDB wrapper

--- js/wrappers/MovieDb.js
+++ js/wrappers/MovieDb.js
@@ -2,17 +2,21 @@ const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'abd0a73367a21a2827c86f7445358c37';
 
 class MovieDb {
-  static searchMovie(options){
+  static searchMovie(options = {}){
     return fetch(buildRequestUrl('search/movie', options))
-      .then(response => response.json());
+      .then((response) => {
+        if(!response.ok) throw response.status;
+        return response.json();
+      })
+      .catch((error) => console.log("Error response from MovieDB API: code " + error));
   }
 }
 
-const buildRequestUrl = (endpoint, options = {}) => {
+const buildRequestUrl = (endpoint = '', options = {}) => {
   const requestUrl = new URL(`${API_URL}${endpoint}`);
   options.api_key = API_KEY;
   Object.keys(options).map(key => requestUrl.searchParams.append(key, options[key]))
-  return requestUrl;
+  return requestUrl.toString();
 }
 
 export default MovieDb;
---
+++ test/MovieDbTest.js
@@ -0,0 +1,52 @@
+import chai  from 'chai';
+import sinon from 'sinon';
+import chaiAsPromised from 'chai-as-promised';
+import fetchMock from 'fetch-mock';
+import fetch from 'node-fetch';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+import MovieDb from '../js/wrappers/MovieDb.js';
+
+describe('MovieDb', function(){
+  describe('.searchMovie', function(){
+    afterEach(() => {
+      fetchMock.restore();
+    });
+
+    context('when no arguments given', function(){
+      it('should throw an error', function(){
+        fetchMock.get("begin:https://api.themoviedb.org/3/",
+          {
+            ok: false,
+            status: 422
+         }).catch(unmatchedUrl => {
+          return fetch(unmatchedUrl)
+        });
+        expect(MovieDb.searchMovie()).to.be.rejectedWith(Error);
+        fetchMock.restore();
+      });
+    });
+
+    context('when query given in options', function(){
+      it('should return a promise with json response', function(){
+        fetchMock.get("begin:https://api.themoviedb.org/3/",
+          {
+            ok: true,
+            status: 200,
+            body: JSON.stringify({"page":1 ,"total_results":1, "total_pages":1, "results":[{"vote_count":2,"id":115268,"video":false,"vote_average":4,"title":"Balu ABCDEFG","popularity":1.000831,"poster_path":"\/gFsFtGnRBOT6BprnmaKnf4tmGKM.jpg","original_language":"en","original_title":"Balu ABCDEFG","genre_ids":[18,10749,10769],"backdrop_path":null,"adult":false,"overview":"Balu ABCDEFG (Telugu: బాలు ABCDEFG) is a Telugu film released on 6 January 2005 and was directed by A. Karunakaran. This film stars Pawan Kalyan, Shriya, and Neha Oberoi. The film was produced by C. Ashwini Dutt under his production company Vyjayanthi Movies. The movie was a good grosser at the box office; it ran for 100 days successfully in 8 theaters in Andhra Pradesh","release_date":"2005-01-06"}]})
+        }).catch(unmatchedUrl => {
+          return fetch(unmatchedUrl)
+        });
+
+        return MovieDb.searchMovie({query: 'abc'})
+          .then(function(data){
+            expect(data.page).to.equal(1);
+            expect(data.total_results).to.equal(1);
+            expect(data.results.length).to.equal(1);
+          });
+      });
+    });
+  });
+});
